39a9d6c15ecdb884d087064af9ae418eeaae0b1c
KevinFunck <129848357+KevinFunck@users.noreply.github.com>
2025-08-27 20:28:32 +0200

    Initial Dash Component created
    This repository contains the initial creation of a custom Dash component. The basic setup and structure are in place, but no functionality has been implemented yet.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
+];
