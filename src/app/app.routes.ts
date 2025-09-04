8478922fe2e4dcffb20b19709d5c9c4eb742cd37
KevinFunck <129848357+KevinFunck@users.noreply.github.com>
2025-09-04 19:28:49 +0200

    Add addcustomer component and style my toolbar in customers component
    -Style my toolbar in customers component with Tailwind
    -Add addcustomer component
    -Add routing in the menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,11 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/dashboard/dashboard/dashboard.component').then(m => m.DashboardComponent)
   },
+   {
+    path: 'customers',
+    loadComponent: () =>
+      import('./pages/customers/customers/customers.component').then(m => m.CustomersComponent)
+  },
   {
     path: '',
     redirectTo: 'dashboard',
